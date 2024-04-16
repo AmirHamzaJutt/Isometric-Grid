b220c13efe68269582fa88c8069e816ce5b18e89
HAMZA_JUTT\ah408 <ah408567@gmail.com>
2024-04-17 02:12:43 +0500

    Making grid

--- Assets/Isomatric Grid/Scripts/Input_System.cs
+++ Assets/Isomatric Grid/Scripts/Input_System.cs
@@ -4,40 +4,33 @@ public class Input_System : MonoBehaviour
 {
     [SerializeField] private Camera _mainCamera;
     [SerializeField] private LayerMask _layer;
-    [SerializeField] private Transform _inaccuracy;
-    [SerializeField] GameObject _snapEffect;
-    [SerializeField] private float _movespeed = 5;
-    [SerializeField] private bool _snap;
-    [Space(10)]
-    [SerializeField] private float _offsetX = 1.0f; // Offset amount for X axis
-    [SerializeField] private float _offsetY = 1.0f;
-    [SerializeField] private float _offsetZ = 1.0f;
-    private Vector3 targetPosition;
-    Vector3 screenCenter;
+    [SerializeField] private float _moveSpeed = 5f;
+    [SerializeField] private float _offsetX = 1f;
+    [SerializeField] private float _offsetY = 1f;
+    [SerializeField] private float _offsetZ = 1f;
 
-    private void Start()
-    {
-    }
     private void Update()
     {
-       // screenCenter = gameController.uiManager.Cursor().position;
-        Ray ray = _mainCamera.ScreenPointToRay(screenCenter);
-        //Debug.DrawRay(ray.origin, ray.direction * 5000f, Color.red);
+        Vector3 mousePosition = Input.mousePosition;
+        Ray ray = _mainCamera.ScreenPointToRay(mousePosition);
         RaycastHit raycastHit;
-        if (Physics.Raycast(ray, out raycastHit, 5000f, _layer))
+        if (Physics.Raycast(ray, out raycastHit, Mathf.Infinity, _layer))
         {
-            if (raycastHit.collider.CompareTag("snap") && _snap)
-            {
-                Debug.LogError("snap");
-                targetPosition = raycastHit.collider.bounds.center;
-                _snapEffect.SetActive(true);
-            }
-            else
-            {
-                targetPosition = new Vector3(raycastHit.point.x + _offsetX, raycastHit.point.y + _offsetY, raycastHit.point.z + _offsetZ);
-                _snapEffect.SetActive(false);
-            }
-            transform.position = Vector3.Lerp(transform.position, targetPosition, _movespeed * Time.deltaTime * 5f);
+            Vector3 targetPosition = new Vector3(raycastHit.point.x + _offsetX, raycastHit.point.y + _offsetY, raycastHit.point.z + _offsetZ);
+            transform.position = Vector3.Lerp(transform.position, targetPosition, _moveSpeed * Time.deltaTime);
+        }
+    }
+    private void OnDrawGizmos()
+    {
+        // Draw the raycast in the scene view
+        if (_mainCamera != null)
+        {
+            Vector3 mousePosition = Input.mousePosition;
+            Ray ray = _mainCamera.ScreenPointToRay(mousePosition);
+
+            Gizmos.color = Color.red;
+            Gizmos.DrawRay(ray.origin, ray.direction * 5000f);
         }
     }
 }
+
